da1afc9d37c2d5aa57e303eeaba4f76846bb39da
gianmarcoian <103632703+gianmarcoian@users.noreply.github.com>
2024-07-23 11:03:00 -0500

    Create inpainting_utils.py

---
+++ app/utils/inpainting_utils.py
@@ -0,0 +1,148 @@
+import torch
+import numpy as np
+import tqdm
+import functools
+from .train_ddpm import get_score_fn
+from .sampling import NoneCorrector, NonePredictor
+
+import logging
+logger = logging.getLogger(__name__)
+
+def shared_predictor_update_fn(x, t, sde, model, predictor, probability_flow, continuous):
+  """A wrapper that configures and returns the update function of predictors."""
+  score_fn = get_score_fn(sde, model, train=False, continuous=continuous)
+  predictor_obj = predictor(sde, score_fn)  #, probability_flow)
+  return predictor_obj.update_fn(x, t)
+
+
+def shared_corrector_update_fn(x, t, sde, model, corrector, continuous, snr, n_steps):
+  """A wrapper tha configures and returns the update function of correctors."""
+  score_fn = get_score_fn(sde, model, train=False, continuous=continuous)
+  corrector_obj = corrector(sde, score_fn, snr, n_steps)
+  return corrector_obj.update_fn(x, t)
+
+
+def get_pc_inpainter(sde, predictor, corrector, inverse_scaler, snr,
+                     n_steps=1, probability_flow=False, continuous=False,
+                     denoise=True, eps=1e-5):
+  logger.debug(f"predictor: {predictor}")
+  logger.debug(f"corrector: {corrector}")
+  predictor_update_fn = functools.partial(shared_predictor_update_fn,
+                                          sde=sde,
+                                          predictor=predictor,
+                                          probability_flow=probability_flow,
+                                          continuous=continuous)
+
+  corrector_update_fn = functools.partial(shared_corrector_update_fn,
+                                          sde=sde,
+                                          corrector=corrector,
+                                          continuous=continuous,
+                                          snr=snr,
+                                          n_steps=n_steps)
+
+  def get_inpaint_update_fn(update_fn):
+    """Modify the update function of predictor & corrector to incorporate data information."""
+
+    def inpaint_update_fn(model, data, mask, x, t):
+      with torch.no_grad():
+        vec_t = torch.ones(data.shape[0], device=data.device) * t
+        x, x_mean = update_fn(x, vec_t, model=model)
+        masked_data_mean, std = sde.marginal_prob(data, vec_t)
+        masked_data = masked_data_mean + torch.randn_like(x) * std[:, None, None, None]
+        x = x * (1. - mask) + masked_data * mask
+        x_mean = x * (1. - mask) + masked_data_mean * mask
+        return x, x_mean
+
+    return inpaint_update_fn
+
+  projector_inpaint_update_fn = get_inpaint_update_fn(predictor_update_fn)
+  corrector_inpaint_update_fn = get_inpaint_update_fn(corrector_update_fn)
+  def pc_inpainter(model, data, mask):
+    logger.debug("entro qui?")
+
+    with torch.no_grad():
+      # Initial sample
+      x = data * mask + sde.prior_sampling(data.shape).to(data.device) * (1. - mask)
+      timesteps = torch.linspace(sde.T, eps, sde.N)
+      for i in tqdm.tqdm(range(sde.N)):
+        t = timesteps[i]
+        x, x_mean = corrector_inpaint_update_fn(model, data, mask, x, t)
+        x, x_mean = projector_inpaint_update_fn(model, data, mask, x, t)
+
+      return inverse_scaler(x_mean if denoise else x)
+
+  return pc_inpainter
+
+
+def get_pc_colorizer(sde, predictor, corrector, inverse_scaler,
+                     snr, n_steps=1, probability_flow=False, continuous=False,
+                     denoise=True, eps=1e-5):
+
+  # `M` is an orthonormal matrix 
+  M = torch.tensor([[5.7735014e-01, -8.1649649e-01, 4.7008697e-08],
+                   [5.7735026e-01, 4.0824834e-01, 7.0710671e-01],
+                   [5.7735026e-01, 4.0824822e-01, -7.0710683e-01]])
+
+  invM = torch.inverse(M)
+
+  # Decouple a gray-scale image with `M`
+  def decouple(inputs):
+    return torch.einsum('bihw,ij->bjhw', inputs, M.to(inputs.device))
+
+  # The inverse function to `decouple`.
+  def couple(inputs):
+    return torch.einsum('bihw,ij->bjhw', inputs, invM.to(inputs.device))
+
+  predictor_update_fn = functools.partial(shared_predictor_update_fn,
+                                          sde=sde,
+                                          predictor=predictor,
+                                          probability_flow=probability_flow,
+                                          continuous=continuous)
+  corrector_update_fn = functools.partial(shared_corrector_update_fn,
+                                          sde=sde,
+                                          corrector=corrector,
+                                          continuous=continuous,
+                                          snr=snr,
+                                          n_steps=n_steps)
+
+  def get_colorization_update_fn(update_fn):
+    """Modify update functions of predictor & corrector to incorporate information of gray-scale images."""
+
+    def colorization_update_fn(model, gray_scale_img, x, t):
+      mask = get_mask(x)
+      vec_t = torch.ones(x.shape[0], device=x.device) * t
+      x, x_mean = update_fn(x, vec_t, model=model)
+      masked_data_mean, std = sde.marginal_prob(decouple(gray_scale_img), vec_t)
+      masked_data = masked_data_mean + torch.randn_like(x) * std[:, None, None, None]
+      x = couple(decouple(x) * (1. - mask) + masked_data * mask)
+      x_mean = couple(decouple(x) * (1. - mask) + masked_data_mean * mask)
+      return x, x_mean
+
+    return colorization_update_fn
+
+  def get_mask(image):
+    mask = torch.cat([torch.ones_like(image[:, :1, ...]),
+                      torch.zeros_like(image[:, 1:, ...])], dim=1)
+    return mask
+
+  predictor_colorize_update_fn = get_colorization_update_fn(predictor_update_fn)
+  corrector_colorize_update_fn = get_colorization_update_fn(corrector_update_fn)
+
+  def pc_colorizer(model, gray_scale_img):
+
+    with torch.no_grad():
+      shape = gray_scale_img.shape
+      mask = get_mask(gray_scale_img)
+      # Initial sample
+      x = couple(decouple(gray_scale_img) * mask + \
+                 decouple(sde.prior_sampling(shape).to(gray_scale_img.device)
+                          * (1. - mask)))
+      timesteps = torch.linspace(sde.T, eps, sde.N)
+      for i in range(sde.N):
+        t = timesteps[i]
+        x, x_mean = corrector_colorize_update_fn(model, gray_scale_img, x, t)
+        x, x_mean = predictor_colorize_update_fn(model, gray_scale_img, x, t)
+
+      return inverse_scaler(x_mean if denoise else x)
+
+  return pc_colorizer
